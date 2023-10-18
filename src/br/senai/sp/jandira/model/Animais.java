13a48a3e3a38962aebc2e6fac49f89654ad367b1
IGOR LUIS DE ANDRADE ARAUJO <23111733@CFP127ALUNO.LOCAL>
2023-10-18 15:31:59 -0300

    relembrando conteudos

--- src/br/senai/sp/jandira/model/Animais.java
+++
@@ -1,8 +0,0 @@
-package br.senai.sp.jandira.model;
-
-public class Animais {
-
-    public void FazerSom (){
-        System.out.println("O animal está fazendo barulho");
-    }
-}
--- src/br/senai/sp/jandira/model/Vaca.java
+++
@@ -1,8 +0,0 @@
-package br.senai.sp.jandira.model;
-
-public class Vaca  extends Animais{
-
-    public void FazerSom(){
-        System.out.println("Muu Muu");
-    }
-}
